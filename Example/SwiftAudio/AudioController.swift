10a2ad213445aa01c96442bc2a35c35f748b4624
jan sebastian <jnsbstn391@gmail.com>
2021-02-24 17:20:19 +0700

    remove call mising file

--- Example/SwiftAudio/AudioController.swift
+++ Example/SwiftAudio/AudioController.swift
@@ -17,8 +17,7 @@ class AudioController {
     let audioSessionController = AudioSessionController.shared
     
     let sources: [AudioItem] = [
-        DefaultAudioItem(audioUrl: "https://www.eclassical.com/custom/eclassical/files/BIS1447-002-flac_24.flac", artist: "Unknow Artist", title: "Unknow Title", albumTitle: "Unknow Album Title", sourceType: .stream, artwork: #imageLiteral(resourceName: "22AMI")),
-        DefaultAudioItem(audioUrl: "\(Bundle.main.path(forResource: "BIS1447-002-flac_24", ofType: "flac")!)", artist: "Unknow Artist - 1", title: "Unknow Title - 1", albumTitle: "Unknow Album -1 ", sourceType: .file, artwork: #imageLiteral(resourceName: "22AMI"))
+        DefaultAudioItem(audioUrl: "https://www.eclassical.com/custom/eclassical/files/BIS1447-002-flac_24.flac", artist: "Unknow Artist", title: "Unknow Title", albumTitle: "Unknow Album Title", sourceType: .stream, artwork: #imageLiteral(resourceName: "22AMI"))
     ]
     
     init() {
